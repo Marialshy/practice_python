e75bf0f71313a2dc306982ac9f6c89aa3908c93c
marialshy <marialshy@gmail.om>
2022-07-17 20:15:02 +0300

    mixed list fixed

--- seminar2/seminar2_hw/sem2_hw5.py
+++ seminar2/seminar2_hw/sem2_hw5.py
@@ -8,19 +8,15 @@ def fill_list_n_range(nmbr: int):
         some_list.append(i)
     return some_list
 print(fill_list_n_range(number))
+
 def list_mix(any_list: list):
-    new_list = any_list
-    index =0
-    index_list=[]
-    
-    for i in range(len(any_list)): 
-        while index_list.count(i) !=1:
-            index_n = random.randint(0,len(any_list)-1)
-            print(index_n)
-            index_list.append(index_n)
-            print(index_list)
-            new_list[i]= any_list[index-1]
-            print(new_list)
+    new_list = []
+    while len(new_list)<len(any_list):
+            index = random.randint(0,len(any_list)-1)
+            if any_list[index] in new_list:
+                pass
+            else:
+                new_list.append(any_list[index])
     return new_list
 
 print(list_mix(fill_list_n_range(number)))
