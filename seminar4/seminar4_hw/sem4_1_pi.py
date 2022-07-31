377fc723208c649bd2957cdc21d24e5009f5e3c5
marialshy <marialshy@gmail.om>
2022-07-31 13:36:40 +0300

    pi find + round, also asfor input format 0.001

--- seminar4/seminar4_hw/sem4_1_pi.py
+++ seminar4/seminar4_hw/sem4_1_pi.py
@@ -5,3 +5,13 @@ with open(path,'r') as pi:
     data_pi = float(pi.read(5+d))
 print(round(data_pi,d))
 
+#если d получен в формате 0.001:
+d = (input("введите число в нужном формате: ")) # или считать с файла например вместо ввода
+path = r'seminar4/seminar4_hw/3,14.txt'
+with open(path,'r') as pi:
+    new_d = str.split(d,".")
+    count = 0
+    for i in range(len(new_d[1])):
+        count +=i
+    data_pi2 = float(pi.read(5+count))
+print(round(data_pi2,count))
