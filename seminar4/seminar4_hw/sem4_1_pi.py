1ea0a2e7a6c7668370e24a8fde92ec2008f54da1
marialshy <marialshy@gmail.om>
2022-07-31 13:11:45 +0300

    pi find

---
+++ seminar4/seminar4_hw/sem4_1_pi.py
@@ -0,0 +1,6 @@
+#Вычислить число c заданной точностью d, 10^{-1} ≤ d ≤10^{-10}
+d = int(input("введите число знаков после запятой: "))
+path = r'seminar4/seminar4_hw/3,14.txt'
+with open(path,'r') as pi:
+    data_pi = float(pi.read(2+d))
+print(data_pi)
