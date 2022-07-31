243c2bccce46518f58861659cdd6e3144ea59c63
marialshy <marialshy@gmail.om>
2022-07-31 13:20:14 +0300

    pi find + round, not only text

--- seminar4/seminar4_hw/sem4_1_pi.py
+++ seminar4/seminar4_hw/sem4_1_pi.py
@@ -2,5 +2,6 @@
 d = int(input("введите число знаков после запятой: "))
 path = r'seminar4/seminar4_hw/3,14.txt'
 with open(path,'r') as pi:
-    data_pi = float(pi.read(2+d))
-print(data_pi)
+    data_pi = float(pi.read(5+d))
+print(round(data_pi,d))
+
