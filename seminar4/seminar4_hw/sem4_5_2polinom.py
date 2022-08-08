2644e4658996e3d285d9545e1cddb598b06bca79
Marialshy <102726683+Marialshy@users.noreply.github.com>
2022-08-08 09:07:13 +0300

    Update sem4_5_2polinom.py
    trying to use S.partition(*x^9[i]) - to find same parts of elements

--- seminar4/seminar4_hw/sem4_5_2polinom.py
+++ seminar4/seminar4_hw/sem4_5_2polinom.py
@@ -15,8 +15,20 @@ def convert_countable(sm_list: list, i:int, split_symb: str=' '):
 print(list_1)
 print(list_2)
 
+#счетчик вида: if len(list_1)>len(list_2):
+#   for i in (len(list_1)), else: for i in (len(list_2))
+
 polymn_1=convert_countable(list_1,0)
 polymn_2=convert_countable(list_2,0)
+
+#new_str = []
+#for i in (0,len(polymn_1)-1):
+#    element_1=str(polymn_1[i]).partition('*x^')
+#    element_2=str(polymn_2[i]).partition('*x^') #разные индексы
+#    if element_1[-1]==element_2[-1]:
+#        new_str.append(int(element_1[0])+int(element_1[0]))
+#    else: pass
+#print(new_str)
 
 # element1=convert_countable(polymn_1,0,'^')
 # element2=convert_countable(polymn_2,0,'^')
